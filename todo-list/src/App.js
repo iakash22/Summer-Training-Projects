7e0643b611fa22f4d49debefc72825ba9f27d765
Akash kumar <94979388+iakash22@users.noreply.github.com>
2023-07-25 11:39:15 +0530

    create todo list app

---
+++ todo-list/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import TodoForm from './components/TodoForm';
+function App() {
+  return (
+    <div className="">
+      <TodoForm />
+    </div>
+  );
+}
+
+export default App;
